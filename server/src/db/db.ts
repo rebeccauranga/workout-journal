83e0ee2ee424aa9ba9fc5a944fe130f63c6dcfba
Rebecca Womack <urangarebecca@gmail.com>
2021-09-17 20:57:01 -0400

    updates server files

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,14 +1,11 @@
 import pool from "./config";
-import { Exercise } from "../models";
+import { Exercise } from "../../../shared/models";
 
 export async function listExercises(): Promise<Exercise[]> {
-  const sql = "SELECT id, name, description, video_url FROM exercises;";
+  const sql = "SELECT id, name, category, description, video_url FROM exercises;";
   const res = await pool.query(sql);
   return res.rows;
 }
-
-
-
 
 // NOTES
 // ddl = data definition language (create table, create database)
