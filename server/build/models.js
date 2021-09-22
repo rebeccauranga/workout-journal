2ce612098507866a6aab3135f7f5bc0bea923042
Rebecca Womack <urangarebecca@gmail.com>
2021-09-21 20:58:28 -0400

    removed build files

--- server/build/models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ExerciseCategory = void 0;
-var ExerciseCategory;
-(function (ExerciseCategory) {
-    ExerciseCategory["LowerBody"] = "Lower Body";
-    ExerciseCategory["UpperBody"] = "Upper Body";
-    ExerciseCategory["Cardio"] = "Cardio";
-})(ExerciseCategory = exports.ExerciseCategory || (exports.ExerciseCategory = {}));
