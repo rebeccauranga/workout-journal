3d65238602df9a21e70649ba77efc2242197404a
Rebecca Womack <urangarebecca@gmail.com>
2021-09-21 21:05:09 -0400

    updates video url naming convention

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -9,7 +9,7 @@ const exercises: Exercise[] = [
     name: "Squats",
     description:
       "A squat is a strength exercise in which the trainee lowers their hips from a standing position and then stands back up. During the descent of a squat, the hip and knee joints flex while the ankle joint dorsiflexes; conversely the hip and knee joints extend and the ankle joint plantarflexes when standing up.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -17,7 +17,7 @@ const exercises: Exercise[] = [
     name: "Barbell deadlifts",
     description:
       "The deadlift is a weight training exercise in which a loaded barbell or bar is lifted off the ground to the level of the hips, torso perpendicular to the floor, before being placed back on the ground. It is one of the three powerlifting exercises, along with the squat and bench press.",
-    videoURL: "https://youtu.be/tNn7AlPITOw",
+    video_url: "https://youtu.be/tNn7AlPITOw",
   },
   {
     id: uuidv4(),
@@ -25,7 +25,7 @@ const exercises: Exercise[] = [
     name: "Barbell hip thrusts",
     description:
       "A barbell hip thrust is a lower-body strength training exercise defined by lifting your lower back and torso with your knees bent and your upper body resting on a bench. With proper form, the barbell hip thrust works muscle groups across your entire lower body, particularly the gluteal muscles.",
-    videoURL: "https://youtu.be/LM8XHLYJoYs",
+    video_url: "https://youtu.be/LM8XHLYJoYs",
   },
   {
     id: uuidv4(),
@@ -33,7 +33,7 @@ const exercises: Exercise[] = [
     name: "Bulgarian split squats",
     description:
       "A type of single-leg squat, the Bulgarian split squat is sure to deliver big benefits to your lower body. With one leg behind you and elevated off of the ground, this exercise targets many of the same muscles as a traditional squat, but with an emphasis on the quads.",
-    videoURL: "https://youtu.be/2C-uNgKwPLE",
+    video_url: "https://youtu.be/2C-uNgKwPLE",
   },
   {
     id: uuidv4(),
@@ -41,7 +41,7 @@ const exercises: Exercise[] = [
     name: "Lunges",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -49,7 +49,7 @@ const exercises: Exercise[] = [
     name: "Curtsy lunges",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -57,7 +57,7 @@ const exercises: Exercise[] = [
     name: "Dumbbell Romanian Deadlifts",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -65,7 +65,7 @@ const exercises: Exercise[] = [
     name: "Leg extension",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -73,7 +73,7 @@ const exercises: Exercise[] = [
     name: "Rows",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -81,7 +81,7 @@ const exercises: Exercise[] = [
     name: "Bicep curls",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -89,7 +89,7 @@ const exercises: Exercise[] = [
     name: "Lat pulldowns",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -97,7 +97,7 @@ const exercises: Exercise[] = [
     name: "Shoulder press",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -105,7 +105,7 @@ const exercises: Exercise[] = [
     name: "Chest press",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -113,7 +113,7 @@ const exercises: Exercise[] = [
     name: "Running",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -121,7 +121,7 @@ const exercises: Exercise[] = [
     name: "Walking",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
   {
     id: uuidv4(),
@@ -129,18 +129,17 @@ const exercises: Exercise[] = [
     name: "Elliptical",
     description:
       "A lunge can refer to any position of the human body where one leg is positioned forward with knee bent and foot flat on the ground while the other leg is positioned behind.",
-    videoURL: "https://youtu.be/U3HlEF_E9fo",
+    video_url: "https://youtu.be/U3HlEF_E9fo",
   },
 ];
 
 
 function seedExercises(exercises: Exercise[]) {
-  exercises.forEach(async ({ id, category, name, description, videoURL }) => {
+  exercises.forEach(async ({ id, category, name, description, video_url }) => {
     const sql =
       "INSERT INTO exercises(id, category, name, description, video_url) VALUES($1, $2, $3, $4, $5) RETURNING *";
-    const values = [id, category, name, description, videoURL];
+    const values = [id, category, name, description, video_url];
     const res = await pool.query(sql, values);
-    console.log(res.rows[0]);
   });
 }
 
