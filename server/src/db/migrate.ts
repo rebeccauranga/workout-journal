60af1a57dbce2bbd14c3822ac6bcc3fd057799a8
Rebecca Womack <urangarebecca@gmail.com>
2021-10-02 17:53:00 -0400

    updates ddl

--- server/src/db/migrate.ts
+++ server/src/db/migrate.ts
@@ -4,14 +4,14 @@ const exercisesDDL = `
     CREATE TABLE IF NOT EXISTS exercises (
         id UUID PRIMARY KEY,
         category VARCHAR(50),
-        name VARCHAR(50),
+        name VARCHAR(50) UNIQUE,
         description TEXT,
         video_url VARCHAR(255)
     )
 `;
 
 const usersDDL = `
-    CREATE TABLE users (
+    CREATE TABLE IF NOT EXISTS users (
       id UUID PRIMARY KEY,
       email VARCHAR(50) UNIQUE NOT NULL,
       photo_url TEXT,
