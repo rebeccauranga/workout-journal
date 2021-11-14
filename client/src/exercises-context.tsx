29e1da52804e444770a7d5fc163cc91a6bebcd54
Rebecca Womack <urangarebecca@gmail.com>
2021-11-14 11:15:22 -0500

    client: creates new context for exercise data

---
+++ client/src/exercises-context.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, createContext, useContext } from "react";
+import { Exercise } from "../../shared/models";
+
+interface ExerciseData {
+  exercises: Exercise[];
+  exercisesById: Map<string, Exercise>;
+}
+
+const useProvideExercises = (): ExerciseData => {
+  const [exercises, setExercises] = useState<Exercise[]>([]);
+
+  async function getExercises() {
+    const url = "/api/exercises";
+    const response = await fetch(url);
+    if (response.status === 200) {
+      const exercises = await response.json();
+      setExercises(exercises);
+    }
+  }
+
+  useEffect(() => {
+    try {
+      getExercises();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  const exercisesById = new Map<string, Exercise>();
+  exercises?.forEach((exercise) => exercisesById.set(exercise.id, exercise));
+
+  return { exercises, exercisesById };
+};
+
+const ExerciseContext = createContext<ExerciseData>({} as ExerciseData);
+
+export const useExercises = () => {
+  return useContext(ExerciseContext);
+};
+
+export const ProvideExercises: React.FC = ({ children }) => {
+  const exerciseData = useProvideExercises();
+  return (
+    <ExerciseContext.Provider value={exerciseData}>
+      {children}
+    </ExerciseContext.Provider>
+  );
+};
